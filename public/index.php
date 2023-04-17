2488d5a6dd9d46b6b9b20db937add364b4c4b8a8
glaz0ff <glazzer0@mail.ru>
2023-04-17 21:28:59 +0500

    Подключил выключатель

--- public/index.php
+++ public/index.php
@@ -34,17 +34,41 @@
         background-color: #fd850d;
         border-color: #fd710d;
     }
+    h1 {
+        font-size: 80px;
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(4,42,192,1) 12%, rgba(253,133,13,1) 100%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
 </style>
 <div>
     <video autoplay="" muted="" loop class="background_video">
         <source src="images/cycle.mp4" type="video/mp4">
     </video>
     <div class="main_menu">
-        <div class="form-check form-switch">
+        <h1>LightOn</h1>
+        <div class="form-check form-switch" style="padding-top: 10px">
                 <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" style="width: 200px; height: 100px">
         </div>
     </div>
 </div>
+
+<script>
+    async function onClick()
+    {
+        let response = await fetch("/switch");
+        response = await response.json();
+    }
+    window.onload = async function onLoad()
+    {
+        let response = await fetch("/switchinfo");
+        response = await response.json();
+        if(response === true)
+            document.querySelector("form-check-input").setAttribute("checked", "true");
+        else
+            document.querySelector("form-check-input").removeAttribute("checked");
+    }
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
